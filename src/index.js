7cb902821717f0088c81702210bf5dfa311faf8c
Tarun Soni <TarunSoni@cedcoss.com>
2022-02-04 19:16:36 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function calculation() {
+    var l=document.getElementById('len').value;
+    var w=document.getElementById('wid').value;
+    z =l*w;
+    p=(2*l)+(2*w);
+    document.getElementById('demo').innerHTML="area" +" "+ z +" " +"mtr";
+    document.getElementById('demo1').innerHTML=Perimeter ="Perimeter" +" "+ p +" " +"mtr";
+
+    
+}
